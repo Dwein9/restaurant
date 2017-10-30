355d93698d642d33edd0fb5aee7d45822972d2d4
dwein9 <dwein9@gmail.com>
2017-10-30 18:28:56 -0400

    pass info through menu components

--- src/menuDetail.js
+++ src/menuDetail.js
@@ -1,18 +1,3 @@
-// import React, { Component } from 'react'
-//
-// class MenuDetail extends Component {
-//
-//   render(){
-//     return (
-//       <div>
-//         <h1>Hi</h1>
-//       </div>
-//     )
-//   }
-// }
-//
-// export default MenuDetail;
-
 import React from 'react';
 
 export default function MenuDetail (props) {
